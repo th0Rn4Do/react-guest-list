3ed7bcbf26fc133e33aad89977c40165099d99a9
Mandla <127042808+th0Rn4Do@users.noreply.github.com>
2023-05-12 16:18:49 +0200

    Add input for first name, last name and checkbox attending

---
+++ src/LastNameControlledComponent.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+export default function LastNameControlledComponent() {
+  const [label, setLabel] = useState('');
+  const [userLastnameInput, setUserLastnameInput] = useState('');
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+    >
+      {label !== '' && <p>Last Name: {label}</p>}
+      <input
+        value={userLastnameInput}
+        onChange={(event) => {
+          setUserLastnameInput(event.currentTarget.value);
+        }}
+      />
+      {userLastnameInput === '' ? (
+        ' please type the last name ⌨️'
+      ) : (
+        <button
+          onClick={() => {
+            setLabel(userLastnameInput);
+          }}
+        >
+          update label
+        </button>
+      )}
+    </form>
+  );
+}
