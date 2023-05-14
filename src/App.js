4cdb1087ac3079bcc72c18d7811aca34b57c520d
Mandla <127042808+th0Rn4Do@users.noreply.github.com>
2023-05-14 18:56:00 +0200

    Restructure the webpage by using only NamesList componenet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import './App.css';
 import CheckboxControlledComponent from './CheckboxControlledComponent';
-import FirstNameControlledComponent from './FirstNameControlledComponent';
 import FullName from './FullName';
-import LastNameControlledComponent from './LastNameControlledComponent';
 import logo from './logo.svg';
 import NamesList from './NamesList';
+import FirstNameControlledComponent from './projectnotes/FirstNameControlledComponent';
+import LastNameControlledComponent from './projectnotes/LastNameControlledComponent';
 
 export default function App() {
   return (
     <div>
-      <h2>Guest details</h2>
-      <FirstNameControlledComponent />
-      <br />
-      <LastNameControlledComponent />
-      <br />
-      <CheckboxControlledComponent />
-      <br />
-      <FullName />
-      <br />
+      <h2>New guest details</h2>
       <NamesList />
-      <br />
       <br />
       <h2>Guest list</h2>
       <br />
